577921d55a9175b2b973ec81364807e4ee6743d4
Andrés <alu0101313511@ull.edu.es>
2021-08-17 21:33:57 +0100

    Per object constraints support is done

--- src/inventory_manager.py
+++ src/inventory_manager.py
@@ -4,6 +4,8 @@
 
 from jetson.inference import detectNet
 from jetson.utils import videoSource
+from os.path import join, dirname, isfile
+import csv
 from typing import Tuple, Dict
 
 
@@ -11,11 +13,11 @@ class ObjectType:
     """
     """
     def __init__(self,
-                 name: str,
-                 constraint: int = 0,
+                 class_name: str,
                  amount: int = 0,
+                 constraint: int = 0,
                  price: float = 0.0) -> None:
-        self.name = name
+        self.class_name = class_name
         self.constraint = constraint
         self.amount = amount
         self.price = price
@@ -26,11 +28,11 @@ class ObjectType:
         return abs(dif) if dif < 0 else 0
 
     def __str__(self) -> str:
-        return f"""\t{self.name.title()}
+        return f"""\t{self.class_name.title()}
         Stored: {self.amount}
         Minimum units: {self.constraint}
         Needed: {self.demand}
-        Total cost: {self.amount * self.price:.2f}$
+        Total cost: {round(self.demand * self.price, 2) if self.price else ""}$
         """
 
 
@@ -42,8 +44,7 @@ class InventoryManager:
                  path2model: str,
                  path2labels: str,
                  sensitivity: float,
-                 input_uri: str,
-                 path2constraints: str) -> None:
+                 input_uri: str) -> None:
         """
         """
         self._network = detectNet(threshold=sensitivity, argv=[
@@ -57,21 +58,39 @@ class InventoryManager:
         self._camera = videoSource(input_uri)
 
         with open(path2labels, "r", newline="") as labels_file:
-            self.object_names: Tuple[str, ...] = tuple(label for label in labels_file if label != "BACKGROUND")
+            self.classes: Tuple[str, ...] = tuple(label for label in labels_file if label != "BACKGROUND")
+
+        self._path2constraints: str = join(dirname(dirname(__file__)),
+                                           "config", ".constraints.csv")
+        if not isfile(self._path2constraints):
+            with open(self._path2constraints, "w", newline="") as csv_file:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerow(["Class", "Constraint"])
+                csv_writer.writerows([[class_name, 0] for class_name in self.classes])
 
     @property
     def inventory(self) -> Dict[str, ObjectType]:
         """
         """
-        result: Dict[str, ObjectType] = {name:ObjectType(name)
-                                         for name in self.object_names}
+        result: Dict[str, ObjectType] = {class_name:ObjectType(class_name)
+                                         for class_name in self.classes}
+        self._update_constraints(result)
+        self._update_prices(result)
         # Count occurrences of each type within list of detected objects.
         for class_index in self._network.Detect(self._camera.Capture()):
             result[self._network.GetClassDesc(class_index)].amount += 1
         return result
 
-    @staticmethod
-    def _update_prices(inventory_data: Dict[str, int]) -> None:
+    def _update_constraints(self, inventory_data: Dict[str, ObjectType]) -> None:
+        """
+        """
+        with open(self._path2constraints, "r", newline="") as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            class_header, constraint_header = next(csv_reader)
+            for row in csv_reader:
+                inventory_data[row[class_header]].constraint = int(row[constraint_header])
+
+    def _update_prices(self, inventory_data: Dict[str, ObjectType]) -> None:
         """
         """
         pass
