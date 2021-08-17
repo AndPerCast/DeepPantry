23b5a0a096ba77409f73a9f08479d9b303abca76
Andrés <alu0101313511@ull.edu.es>
2021-08-17 11:37:19 +0100

    Basic source code files

--- config/run_app.sh
+++ config/run_app.sh
@@ -2,8 +2,9 @@
 
 # Utility script to execute main app, inside a container.
 
-sudo docker run --runtime nvidia -it --rm --network host \
-                --volume ~/DeepPantry:/DeepPantry \
-                --volume /tmp/argus_socket:/tmp/argus_socket \
-                --device /dev/video0 \
-                dustynv/jetson-inference:r32.6.1
+# Add sudo priviledges if needed.
+docker run --runtime nvidia -it --rm --network host \
+           --volume ~/DeepPantry:/DeepPantry \
+           --volume /tmp/argus_socket:/tmp/argus_socket \
+           --device /dev/video0 \
+           dustynv/jetson-inference:r32.6.1
