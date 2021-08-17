7dbc2c79f48b3e20ce793c47206165e477514e2b
Andrés <alu0101313511@ull.edu.es>
2021-08-17 20:00:52 +0100

    Developing object constraints

--- src/inventory_manager.py
+++ src/inventory_manager.py
@@ -12,19 +12,25 @@ class ObjectType:
     """
     def __init__(self,
                  name: str,
+                 constraint: int = 0,
                  amount: int = 0,
-                 demand: int = 0,
                  price: float = 0.0) -> None:
         self.name = name
+        self.constraint = constraint
         self.amount = amount
-        self.demand = demand
         self.price = price
+
+    @property
+    def demand(self) -> int:
+        dif = self.amount - self.constraint
+        return abs(dif) if dif < 0 else 0
 
     def __str__(self) -> str:
         return f"""\t{self.name.title()}
-        Units: {self.amount}
+        Stored: {self.amount}
+        Minimum units: {self.constraint}
         Needed: {self.demand}
-        Total cost: {self.amount * self.price}
+        Total cost: {self.amount * self.price:.2f}$
         """
 
 
@@ -36,7 +42,8 @@ class InventoryManager:
                  path2model: str,
                  path2labels: str,
                  sensitivity: float,
-                 input_uri: str) -> None:
+                 input_uri: str,
+                 path2constraints: str) -> None:
         """
         """
         self._network = detectNet(threshold=sensitivity, argv=[
@@ -50,22 +57,18 @@ class InventoryManager:
         self._camera = videoSource(input_uri)
 
         with open(path2labels, "r", newline="") as labels_file:
-            self._object_names: Tuple[str, ...] = tuple(label for label in labels_file if label != "BACKGROUND")
+            self.object_names: Tuple[str, ...] = tuple(label for label in labels_file if label != "BACKGROUND")
 
     @property
-    def inventory(self) -> Dict[str, int]:
+    def inventory(self) -> Dict[str, ObjectType]:
         """
         """
-        result: Dict[str, int] = {name:0 for name in self._object_names}
+        result: Dict[str, ObjectType] = {name:ObjectType(name)
+                                         for name in self.object_names}
         # Count occurrences of each type within list of detected objects.
         for class_index in self._network.Detect(self._camera.Capture()):
-            result[self._network.GetClassDesc(class_index)] += 1
+            result[self._network.GetClassDesc(class_index)].amount += 1
         return result
-
-    def _check_contraints(self, inventory_data: Dict[str, int]) -> None:
-        """
-        """
-        pass
 
     @staticmethod
     def _update_prices(inventory_data: Dict[str, int]) -> None:
