d26a267e01794685b9fb6f5df58a16f71d353abe
Andrés <alu0101313511@ull.edu.es>
2021-08-18 15:47:32 +0100

    Basic structure for price scraper

--- config/run_app.sh
+++ config/run_app.sh
@@ -3,7 +3,7 @@
 # Utility script to execute main app, inside a container.
 
 # Add sudo priviledges if needed.
-docker run --runtime nvidia -it --rm --network host \
+docker run -d --runtime nvidia -it --rm --network host \
            --volume ~/DeepPantry:/DeepPantry \
            --volume /tmp/argus_socket:/tmp/argus_socket \
            --device /dev/video0 \
---
+++ src/price_scraper.py
@@ -0,0 +1,11 @@
+"""
+"""
+
+from bs4 import BeautifulSoup
+import requests
+from typing import Tuple
+
+SOURCE_URL: str = ""
+
+def scrape_price(product_name: str) -> Tuple[float, str]:
+    return 0.0, ""
