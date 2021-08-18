db00fef580b8bd03e0d70fea5b9b3fdaf48f0c68
Andrés <alu0101313511@ull.edu.es>
2021-08-18 18:48:55 +0100

    Price scraper works with input products

--- src/price_scraper.py
+++ src/price_scraper.py
@@ -3,9 +3,23 @@
 
 from bs4 import BeautifulSoup
 import requests
-from typing import Tuple
+from typing import Tuple, List
 
-SOURCE_URL: str = ""
+SOURCE_URL: str = "https://www.trolley.co.uk"
 
-def scrape_price(product_name: str) -> Tuple[float, str]:
-    return 0.0, ""
+def scrape_price(product_name: str,
+                 parser: str = "html.parser") -> Tuple[str, float, str]:
+    """
+    """
+    url: str = f"{SOURCE_URL}/search/?q={product_name}"
+    soup = BeautifulSoup(requests.get(url).text, parser)
+    product_data: List[Tuple[str, float, str]] = []
+    for product in soup.find_all("div", class_="product-listing"):
+        link: str = SOURCE_URL + product.a["href"]
+        price_str = str(product.a.find("div", class_="_price").contents[0])
+        price = float(price_str[1:])
+        currency = price_str[0]
+        product_data.append((link, price, currency))
+    return min(product_data, key=lambda element: element[1], default=tuple())
+
+print(scrape_price("pineapple"))
