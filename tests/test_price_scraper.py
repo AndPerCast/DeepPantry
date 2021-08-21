3e8e268f6eaebdb67f87950af591b6fd7f179afd
Andrés <alu0101313511@ull.edu.es>
2021-08-21 12:01:28 +0100

    Unit testing for price_scraper is finished

--- tests/test_price_scraper.py
+++ tests/test_price_scraper.py
@@ -24,11 +24,14 @@ class TestPriceScraper(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.html_text: str = ""
         with open(HTML_TEST_FILE, "r", newline="", encoding="utf-8") as f:
             cls.html_text = f.read()
         cls.product_name: str = basename(HTML_TEST_FILE).split("-")[0]
-        cls.price_info = ('https://www.trolley.co.uk/product/morrisons-savers-honey/IBN007', 0.69, '£')
+        cls.price_info = (
+            'https://www.trolley.co.uk/product/morrisons-savers-honey/IBN007',
+            0.69,
+            '£',
+        )
         cls.default_price_info = ("", 0.0, "")
 
     def setUp(self) -> None:
@@ -36,13 +39,14 @@ class TestPriceScraper(unittest.TestCase):
             self.skipTest("Cannot read any content from html test file")
 
     def test_scrape_price(self) -> None:
+        # Check if correct price information is retrieved.
         self.assertTupleEqual(self.price_info, scrape_price(self.product_name))
 
         with patch("price_scraper.requests.get") as mocked_get:
+            # Check if connection related errors induce default values.
             r = requests.Response()
             r.status_code = 500
             mocked_get.return_value = r
-
             self.assertTupleEqual(self.default_price_info,
                                   scrape_price(self.product_name))
 
@@ -54,15 +58,14 @@ class TestPriceScraper(unittest.TestCase):
             self.assertTupleEqual(self.default_price_info,
                                   scrape_price(self.product_name))
             
-            with patch("price_scraper.min") as mocked_min:
-                mocked_get.reset_mock(side_effect=True)
-                requests.Response.text = "<html></html>"
-                r = requests.Response()
-                r.status_code = 200
-                mocked_get.return_value = r
-                self.assertTupleEqual(self.default_price_info,
-                                      scrape_price(self.product_name))
-                mocked_min.assert_called_once_with([])
+            # Check if a lack of products induces default values.
+            mocked_get.reset_mock(side_effect=True)
+            requests.Response.text = "<html></html>"
+            r = requests.Response()
+            r.status_code = 200
+            mocked_get.return_value = r
+            self.assertTupleEqual(self.default_price_info,
+                                  scrape_price(self.product_name))
             
 
 if __name__ == "__main__":
