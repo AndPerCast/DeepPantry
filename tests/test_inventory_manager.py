7035e3dca342ef2fefd518708c2a29c094cd744e
Andrés <alu0101313511@ull.edu.es>
2021-08-22 13:11:26 +0100

    Unit testing for inventory_manager is done

--- tests/test_inventory_manager.py
+++ tests/test_inventory_manager.py
@@ -39,31 +39,31 @@ class TestInventoryManager(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
-        remove(cls.path2labels)
-        remove(InventoryManager._PATH2CONSTRAINTS)
+        if isfile(cls.path2labels):
+            remove(cls.path2labels)
+        if isfile(InventoryManager._PATH2CONSTRAINTS):
+            remove(InventoryManager._PATH2CONSTRAINTS)
 
     def setUp(self) -> None:
-        mocket_detectnet = patch("inventory_manager.detectNet")
-        self.addCleanup(mocket_detectnet.stop)
-        mocket_detectnet = mocket_detectnet.start()
+        self.mocket_detectnet = patch("inventory_manager.detectNet")
+        self.addCleanup(self.mocket_detectnet.stop)
+        self.mocket_detectnet = self.mocket_detectnet.start()
 
-        mocket_video = patch("inventory_manager.videoSource")
-        self.addCleanup(mocket_video.stop)
-        mocket_video = mocket_video.start()
+        self.mocket_video = patch("inventory_manager.videoSource")
+        self.addCleanup(self.mocket_video.stop)
+        self.mocket_video = self.mocket_video.start()
 
         self.manager = InventoryManager("", self.path2labels, "")
 
     def test_inventory(self) -> None:
         with patch("inventory_manager.InventoryManager._update_constraints"), \
-             patch("inventory_manager.InventoryManager._update_prices"), \
-             patch("inventory_manager.videoSource.Capture"), \
-             patch("inventory_manager.detectNet.Detect") as mocked_detect, \
-             patch("inventory_manager.detectNet.GetClassDesc") as mocked_desc:
-
+             patch("inventory_manager.InventoryManager._update_prices"):
+            # Generate an inventory with random number of product units.
             sample_data = self.inventory_data.copy()
             for product in sample_data.values():
                 product.amount = randint(1, 5)
 
+            # Generate a sample list of objects detected by an AI model.
             detections: List[detectNet.Detection] = []
             for index, class_name in enumerate(self.classes):
                 obj = detectNet.Detection()
@@ -71,29 +71,36 @@ class TestInventoryManager(unittest.TestCase):
                 product = sample_data[class_name]
                 detections.extend([obj] * product.amount)
 
-            mocked_detect.return_value = detections
-            mocked_desc.side_effect = lambda id: self.classes[id]
+            # Set up mocks' behaviour, note "()" to use upper level mocks.
+            self.mocket_detectnet().Detect.return_value = detections
+            self.mocket_detectnet().GetClassDesc.side_effect = lambda id: self.classes[id]
             self.assertDictEqual(sample_data, self.manager.inventory)
 
-
     def test_update_constraints(self) -> None:
+        # Check if a default constraints file was created.
+        self.assertTrue(isfile(InventoryManager._PATH2CONSTRAINTS))
+        # Check if default constraint are loaded correctly.
         sample_data1 = self.inventory_data.copy()
         self.manager._update_constraints(sample_data1)
         self.assertDictEqual(self.inventory_data, sample_data1)
 
+        # Generate a file with random constraints per product.
         sample_data2 = self.inventory_data.copy()
         for product in sample_data2.values():
             product.constraint = randint(1, 5)
-        self.assertTrue(isfile(InventoryManager._PATH2CONSTRAINTS))
         with open(InventoryManager._PATH2CONSTRAINTS, "w", newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(["Class", "Constraint"])
             csv_writer.writerows([[key, value.constraint]
                                   for key, value in sample_data2.items()])
 
+        # Check if non-default constraints are loaded correctly.
         sample_data3 = self.inventory_data.copy()
         self.manager._update_constraints(sample_data3)
         self.assertDictEqual(sample_data2, sample_data3)
+
+        # Reset constraints file.
+        remove(InventoryManager._PATH2CONSTRAINTS)
 
 
 if __name__ == "__main__":
